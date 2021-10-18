d767984a8800957a7e545696469b0baf4c1dcb60
Abhishek Singh <nikku.net@gmail.com>
2021-10-18 19:54:08 +0530

    v1.0.0

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import React, { Component }  from 'react';
+
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+
+import {useEffect} from 'react';
+
+function App() {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [])
+
+  return (
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position="static" color="inherit"> 
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+               <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={7}>
+               <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+}
+
+export default App;
